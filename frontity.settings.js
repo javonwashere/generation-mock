868d2da5b1caada3583faec73ad112c8e23e75e3
javonwashere <javonmcgilberry@gmail.com>
2020-10-12 13:54:24 -0400

    init commit

---
+++ frontity.settings.js
@@ -0,0 +1,59 @@
+import { truncate } from "fs";
+
+const settings = {
+  "name": "gen-mock",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@awsmin/f1",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": true,
+            "showOnPost": true
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://generationmock.com/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
